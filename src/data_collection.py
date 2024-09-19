84855c88f699ab8d53ba4e8b934922ba2ebae264
Teodor469 <lifetroll6@gmail.com>
2024-09-19 15:29:16 +0300

    deleted main dir and moved code to data_collection

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,12 +1,6 @@
 #Separate file for scraping data from yfinance
 
-import numpy as np
-import pandas as pd
 import yfinance as yf
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
 
 def fetch_stock_data(ticker):
